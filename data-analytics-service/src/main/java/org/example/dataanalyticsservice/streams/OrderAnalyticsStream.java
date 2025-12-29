038676437982b58f1d3538e1910730514855227d
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-12-29 11:01:59 +0100

    service data-analytics

---
+++ data-analytics-service/src/main/java/org/example/dataanalyticsservice/streams/OrderAnalyticsStream.java
@@ -0,0 +1,48 @@
+
+package org.example.analyticsservice.streams;
+
+import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.KeyValue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.support.serializer.JsonSerde;
+import org.springframework.stereotype.Component;
+import org.example.orderservice.events.OrderCreatedEvent;
+
+import java.time.Duration;
+
+@Component
+public class OrderAnalyticsStream {
+
+    @Bean
+    public KStream<String, OrderCreatedEvent> kStream(StreamsBuilder builder) {
+        KStream<String, OrderCreatedEvent> stream = builder.stream("orders-topic",
+                Consumed.with(new Serdes.StringSerde(), new JsonSerde<>(OrderCreatedEvent.class)));
+
+        TimeWindows window = TimeWindows.ofSizeWithNoGrace(Duration.ofSeconds(5));
+
+        KTable<Windowed<String>, Long> orderCount = stream
+                .groupByKey()
+                .windowedBy(window)
+                .count();
+
+        KTable<Windowed<String>, Double> orderTotal = stream
+                .groupByKey()
+                .windowedBy(window)
+                .aggregate(
+                        () -> 0.0,
+                        (key, order, aggregate) -> aggregate + order.getTotalPrice(),
+                        Materialized.with(Serdes.String(), Serdes.Double())
+                );
+
+        orderCount.toStream()
+                .map((windowedKey, count) -> KeyValue.pair(windowedKey.key(), count))
+                .to("orders-count-topic", Produced.with(Serdes.String(), Serdes.Long()));
+
+        orderTotal.toStream()
+                .map((windowedKey, total) -> KeyValue.pair(windowedKey.key(), total))
+                .to("orders-total-topic", Produced.with(Serdes.String(), Serdes.Double()));
+
+        return stream;
+    }
+}
---
+++ order-service/src/main/java/org/example/orderservice/events/OrderCreatedEvent.java
@@ -0,0 +1,15 @@
+package org.example.orderservice.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderCreatedEvent {
+    private String orderId;
+    private String productId;
+    private int quantity;
+    private double totalPrice;
+}
